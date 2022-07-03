636c72fe29bf88df395334accc71082381711055
rany <ranygh@riseup.net>
2022-07-03 08:09:09 +0300

    Revert "mm/vmalloc.c: don't dereference possible NULL pointer in __vunmap()"
    * You may keep it in case you like to have a device that doesn't boot.
    
    This reverts commit 5fb539e163668d6318121ef0cd7ba5f5ceb25e16.

--- mm/vmalloc.c
+++ mm/vmalloc.c
@@ -2217,8 +2217,8 @@ static void __vunmap(const void *addr, int deallocate_pages)
 			addr))
 		return;
 
-	area = find_vm_area(addr);
-	if (unlikely(!area)) {
+	va = find_vmap_area((unsigned long)addr);
+	if (unlikely(!va || !(va->flags & VM_VM_AREA))) {
 		WARN(1, KERN_ERR "Trying to vfree() nonexistent vm area (%p)\n",
 				addr);
 		return;
